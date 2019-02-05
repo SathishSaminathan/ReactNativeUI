197cf52c1568c39cd881d65c95993541f35d4b89
SathishSaminathan <Sathishcse1975@gmail.com>
2019-02-05 19:06:15 +0530

    commit_for_mcard_implementation

--- app/components/playground.js
+++ app/components/playground.js
@@ -1,22 +1,36 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Alert } from "react-native";
 
-import MBubble from './MBubble';
-import MPage from './MPage';
+import MBubble from "./MBubble";
+import MPage from "./MPage";
+import MCard from "./MCard";
+import Colors from "../assets/styles/Colors";
+import MButton from "./MButton";
 
 export default class PlayGround extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
+
+  _sample=()=>{
+    Alert.alert("hai")
+  }
+  _sample1=()=>{
+    Alert.alert("Tamil")
+  }
+
 
   render() {
     return (
-      <View style={{
-        flex:1,
-      }}>
-        {/* <View style={{
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: Colors.lightgrey,
+          // padding: 5
+        }}
+      >
+        <View style={{
           flex:1,
           justifyContent:'space-around'
         }}>
@@ -24,9 +38,38 @@ export default class PlayGround extends Component {
         <MBubble status="UnRead"  />
         <MBubble status="Read" action="Delete" />
         <MBubble status="Read" action="More" />
-        </View> */}
+        </View>
 
-        <MPage button="Back" title="Notifications" subtitle="Create an account to continue Create an account to continue.."/>
+        {/* <MButton type="CircularIcon" icon="Save" onPress={()=>this._sample()}/>
+        <MButton type="ActionButton" icon="Save" onPress={()=>this._sample1()}/> */}
+
+        {/* <MPage button="Back" title="Notifications" subtitle="Create an account to continue Create an account to continue.."/> */}
+
+
+        {/* <View
+          style={{
+            flex: 0.9,
+            flexDirection: "column",
+            justifyContent: "space-between"
+          }}
+        >
+          <MCard type="Small" />
+          <MCard type="Medium" />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "space-between",
+            paddingTop: 10
+          }}
+        >
+          <MCard type="Card" />
+          <MCard type="CardLong" />
+        </View> */}
+        {/* <MCard type="Large"/> */}
+        {/* <MCard type="XLarge"/> */}
+
       </View>
     );
   }
